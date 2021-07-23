f48770ff239e9601fa35d820c31586fdd7133185
SmartGlaxx <52786384+SmartGlaxx@users.noreply.github.com>
2021-07-23 11:26:24 +0100

    Second Commit

---
+++ build/precache-manifest.8c6a5fe50d1bcf2b764c2938e93ec39b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0458a6c8d1ab8bdf9e7ac7ef21682522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f197f377462fc90d58f",
+    "url": "/static/css/2.a82c9e4e.chunk.css"
+  },
+  {
+    "revision": "73202fc7550e31a6b825",
+    "url": "/static/css/main.78c62527.chunk.css"
+  },
+  {
+    "revision": "0f197f377462fc90d58f",
+    "url": "/static/js/2.2c1f0d8f.chunk.js"
+  },
+  {
+    "revision": "34c22f11ad7450bfa488f22e64a9886b",
+    "url": "/static/js/2.2c1f0d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73202fc7550e31a6b825",
+    "url": "/static/js/main.e5e811ff.chunk.js"
+  },
+  {
+    "revision": "12485ce19d8e0bc8612f",
+    "url": "/static/js/runtime-main.66c39a7c.js"
+  },
+  {
+    "revision": "febc58d38d211b214797ae4f5ed3dd90",
+    "url": "/static/media/Node.febc58d3.jpg"
+  },
+  {
+    "revision": "594d23279f8289482fec26e61e5bc0ea",
+    "url": "/static/media/appstore.594d2327.png"
+  },
+  {
+    "revision": "7885e72019e8171eb16c27a753ae676c",
+    "url": "/static/media/backgnd.7885e720.jpg"
+  },
+  {
+    "revision": "487511e754834bdf2e6771376d59707e",
+    "url": "/static/media/flutter.487511e7.jpg"
+  },
+  {
+    "revision": "43fa74a696cc80748f093c4f96336ffd",
+    "url": "/static/media/lady2.43fa74a6.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d493f1d23a844b5e7ccc9239c1ea76bf",
+    "url": "/static/media/logo.d493f1d2.png"
+  },
+  {
+    "revision": "09ffdef2a348ff00de8585ddc70cb2f6",
+    "url": "/static/media/man.09ffdef2.png"
+  },
+  {
+    "revision": "d7f85de0f08e57db3b3dce64926481de",
+    "url": "/static/media/python.d7f85de0.jpg"
+  },
+  {
+    "revision": "adad73b364af493c1ef71e7d8c141be6",
+    "url": "/static/media/react.adad73b3.png"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
